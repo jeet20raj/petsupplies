9c38e0311d00302aace505689e838772e8234f1c
jeet20raj <jeetendra.rajput15@gmail.com>
2015-06-17 00:43:18 +0530

    Update ProductVO.java

--- webshop/webshop-admin/src/main/java/com/petsupplies/webshop/admin/model/ProductVO.java
+++ webshop/webshop-admin/src/main/java/com/petsupplies/webshop/admin/model/ProductVO.java
@@ -9,6 +9,13 @@ import org.primefaces.model.SelectableDataModel;
 import com.example.petsupplies.core.backend.entity.ProductEntity;
 import com.petsupplies.webshop.admin.ProductHolder;
 
+/**
+ * ProductVO is decorated class having ProductEntity implements SelectableDataModel to be displayed in the data table
+ * @author Jeetendra
+ * @version 1.0
+ * @since 2015-06-14
+ */
+
 public class ProductVO implements Serializable, SelectableDataModel<ProductVO>{
   
   private ProductEntity productEntity;
@@ -39,11 +46,19 @@ public class ProductVO implements Serializable, SelectableDataModel<ProductVO>{
   public String getProductName() {
     return this.getProductEntity().getProductName();
   }
-
+  /**
+   * Returns the row key used in data table to chose what row was selected
+   * @param 
+   * @return      Row key
+   */ 
   public Object getRowKey(ProductVO productVO) {
     return productVO.getProductEntity().getProductName();
   }
-
+  /**
+   * Returns the selected row data using row key
+   * @param 
+   * @return      Object slected as row
+   */
   public ProductVO getRowData(String string) {
     for (ProductVO productVO : productHolder.getProducts()) {
       if (productVO.getProductEntity().getProductName().equals(this.getProductEntity().getProductName())) {
