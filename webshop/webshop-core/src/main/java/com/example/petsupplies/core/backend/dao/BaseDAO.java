af19346184c58fe3423c147986c94283077d8efe
jeet20raj <jeetendra.rajput15@gmail.com>
2015-06-16 23:28:37 +0530

    Update BaseDAO.java

--- webshop/webshop-core/src/main/java/com/example/petsupplies/core/backend/dao/BaseDAO.java
+++ webshop/webshop-core/src/main/java/com/example/petsupplies/core/backend/dao/BaseDAO.java
@@ -5,7 +5,11 @@ import java.util.logging.Logger;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 
-
+/**
+ * @param product
+ * @return boolean
+ * This method is used to edit product from the DB
+ */
 public abstract class BaseDAO {
   @Inject
   protected EntityManager entityManager;
