255c7abf5167f5319f7c9cf0c0edc7cfd5753942
jeet20raj <jeetendra.rajput15@gmail.com>
2015-06-17 00:47:39 +0530

    Update CategoryVO.java

--- webshop/webshop-admin/src/main/java/com/petsupplies/webshop/admin/model/CategoryVO.java
+++ webshop/webshop-admin/src/main/java/com/petsupplies/webshop/admin/model/CategoryVO.java
@@ -9,6 +9,13 @@ import org.primefaces.model.SelectableDataModel;
 import com.example.petsupplies.core.backend.entity.CategoryEntity;
 import com.petsupplies.webshop.admin.CategoryHolder;
 
+/**
+ * CategoryVO is decorated class having ProductEntity implements SelectableDataModel to be displayed in the data table
+ * @author Jeetendra
+ * @version 1.0
+ * @since 2015-06-14
+ */
+
 public class CategoryVO implements Serializable, SelectableDataModel<CategoryVO>{
   
   private CategoryEntity  categoryEntity ;
@@ -36,11 +43,22 @@ public CategoryEntity getCategoryEntity() {
 	return categoryEntity;
 }
 
-  
+  /**
+	   * Returns the row key used in data table to chose what row was selected
+	   * @param 
+	   * @return      Row key
+	   */
 
   public Object getRowKey(CategoryVO categoryVO) {
     return categoryVO.getCategoryEntity().getCategoryName();
   }
+
+	/**
+	 * Returns the selected row data using row key
+	 * 
+	 * @param
+	 * @return Object slected as row
+	 */
 
   public CategoryVO getRowData(String string) {
     for (CategoryVO categoryVO : categoryHolder.getCategoryVOs()) {
