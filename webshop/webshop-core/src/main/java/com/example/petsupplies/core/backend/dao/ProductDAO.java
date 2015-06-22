b0dd427ae3c262eb40d6110b9b1dad3578265e9b
jeet20raj <jeetendra.rajput15@gmail.com>
2015-06-23 01:15:01 +0530

    Update ProductDAO.java

--- webshop/webshop-core/src/main/java/com/example/petsupplies/core/backend/dao/ProductDAO.java
+++ webshop/webshop-core/src/main/java/com/example/petsupplies/core/backend/dao/ProductDAO.java
@@ -4,7 +4,10 @@ import java.util.List;
 
 import com.example.petsupplies.core.backend.entity.ProductEntity;
 import com.example.petsupplies.core.model.ProductSearchFilter;
-
+/**
+ * @author Jeetendra
+ * ProductDAO exposing the methods to add/edit/delete/display the products.
+ */
 public interface ProductDAO {
   public List<ProductEntity> getProducts(ProductSearchFilter searchFilter);
   
