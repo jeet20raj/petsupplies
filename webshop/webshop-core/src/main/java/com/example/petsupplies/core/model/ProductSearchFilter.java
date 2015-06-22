ec01349f788a5d10b2ff7f144cf2b544dd62797d
jeet20raj <jeetendra.rajput15@gmail.com>
2015-06-23 01:28:36 +0530

    Update ProductSearchFilter.java

--- webshop/webshop-core/src/main/java/com/example/petsupplies/core/model/ProductSearchFilter.java
+++ webshop/webshop-core/src/main/java/com/example/petsupplies/core/model/ProductSearchFilter.java
@@ -1,5 +1,8 @@
 package com.example.petsupplies.core.model;
-
+/**
+ * @author Jeetendra
+ * ProductSearchFilter is having all the search parameters to filter products.
+ */
 public class ProductSearchFilter {
 
 }
