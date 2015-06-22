24595586a263cfce8122e07bfd32c550b57edb62
jeet20raj <jeetendra.rajput15@gmail.com>
2015-06-23 01:16:03 +0530

    Update UserDAO.java

--- webshop/webshop-core/src/main/java/com/example/petsupplies/core/backend/dao/UserDAO.java
+++ webshop/webshop-core/src/main/java/com/example/petsupplies/core/backend/dao/UserDAO.java
@@ -1,7 +1,10 @@
 package com.example.petsupplies.core.backend.dao;
 
 import com.example.petsupplies.core.backend.entity.UserEntity;
-
+/**
+ * @author Jeetendra
+ * UserDAO exposing the method to login and create the user.
+ */
 public interface UserDAO {
   public UserEntity login(String userName, String password);
   public UserEntity createUser(UserEntity userEntity);
