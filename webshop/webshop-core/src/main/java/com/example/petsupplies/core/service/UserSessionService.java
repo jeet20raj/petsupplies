df2b61cdc39a9fdf214acd9fcb2b397b07a3b0e3
jeet20raj <jeetendra.rajput15@gmail.com>
2015-06-23 01:30:25 +0530

    Update UserSessionService.java

--- webshop/webshop-core/src/main/java/com/example/petsupplies/core/service/UserSessionService.java
+++ webshop/webshop-core/src/main/java/com/example/petsupplies/core/service/UserSessionService.java
@@ -2,7 +2,11 @@ package com.example.petsupplies.core.service;
 
 import com.example.petsupplies.core.backend.entity.UserEntity;
 import com.example.petsupplies.core.exceptions.WebshopException;
-
+/**
+ * @author Jeetendra
+ * UserSessionService is used expose methods to login into the system 
+ * Its also used to create user and user address.
+ */
 public interface UserSessionService {
   public UserEntity login(String userName, String password);
 
