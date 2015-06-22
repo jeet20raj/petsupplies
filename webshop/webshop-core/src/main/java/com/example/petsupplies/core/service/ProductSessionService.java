28ed7162e388317479740564208b53bb153d32d7
jeet20raj <jeetendra.rajput15@gmail.com>
2015-06-23 01:29:51 +0530

    Update ProductSessionService.java

--- webshop/webshop-core/src/main/java/com/example/petsupplies/core/service/ProductSessionService.java
+++ webshop/webshop-core/src/main/java/com/example/petsupplies/core/service/ProductSessionService.java
@@ -4,7 +4,10 @@ import java.util.List;
 
 import com.example.petsupplies.core.backend.entity.ProductEntity;
 import com.example.petsupplies.core.model.ProductSearchFilter;
-
+/**
+ * @author Jeetendra
+ * ProductSessionService is used expose methods to search/add/edit/delete products.
+ */
 public interface ProductSessionService {
 
   public List<ProductEntity> getProducts(ProductSearchFilter searchFilter);
